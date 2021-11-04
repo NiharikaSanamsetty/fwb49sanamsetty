dfebf8caf719acc86eac9e2a39af1e5bb7bd027a
NiharikaSanamsetty <41267506+NiharikaSanamsetty@users.noreply.github.com>
2021-11-03 21:58:01 -0500

    committed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ballRouter = require('./routes/ball');
+var addmodsRouter = require('./routes/addmods');
 
 
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ball', ballRouter);
+app.use('/addmods', addmodsRouter);
 
 
 
